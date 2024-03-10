a062e9f8a66363b48c09154ad05d92d1e5d0ec0d
thihatheinhtutlu <linae0343@gmail.com>
2024-03-10 22:41:54 +0630

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Navbar from '../src/scenes/navbar';
+import { SelectedPage } from './shared/types';
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+
+  return (
+    <div className='app bg-gray-20'>
+      <Navbar
+        selectedPage={selectedPage}   
+        setSelectedPage={setSelectedPage}
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "gray-20": "#F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCCC",
+        "gray-500": "#5E0000",
+        "primary-100": "#FFE1E0",
+        "primary-300": "#FFA6A3",
+        "primary-500": "#FF6B66",
+        "secondary-400": "#FFCD58",
+        "secondary-500": "#FFC132",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred": "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assests/images/HomePageGraphic.png')",
+      }),
+      fontFamily: {
+        dmans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serief"],
+      },
+      content: {
+        evolvetext: "url('./assests/images/EvolveText.png')",
+        abstractwaves: "url('./assests/images/AbstractWaves.png')",
+        sparkles: "url('./assests/images/Sparkles.png')",
+        circles: "url('./assests/images/Circles.png')",
+      }
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    }
+  },
+  plugins: [],
+}
