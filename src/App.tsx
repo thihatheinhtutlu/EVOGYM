2188d2b5a48303a92980648200366a3e0627a06d
thihatheinhtutlu <linae0343@gmail.com>
2024-03-15 15:18:30 +0630

    finished ContactUs page as well as cool functionality and fully responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import Home from './scenes/home';
 import Navbar from '../src/scenes/navbar';
 import Benefits from './scenes/benefits';
+import OurClasses from './scenes/ourClasses';
+import ContactUs from './scenes/contactUs';
 import { SelectedPage } from './shared/types';
 
 function App() {
@@ -29,6 +31,8 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage}/>
       <Benefits setSelectedPage={setSelectedPage}/>
+      <OurClasses setSelectedPage={setSelectedPage}/>
+      <ContactUs setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
